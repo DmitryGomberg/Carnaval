ddf489867a015bf25206bd7654046fa9df721782
Dmitry <dpgomberg@gmail.com>
2021-03-18 21:06:33 +0300

    Сверстана третяя секция

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
        $('.menu__icon, .menu__list').toggleClass('active');
        $('body').toggleClass('lock');
    });
+   $('.s3__more').click(function(event){
+      $('.spec').removeClass('hide');
+      $('.s3__more').addClass('hide');
+   });
 });   

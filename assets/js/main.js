b11735ba2f149a544386b537936894ba382ed478
Dmitry <dpgomberg@gmail.com>
2021-03-21 22:22:13 +0300

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,57 @@ $(document).ready(function(){
       $('.spec').removeClass('hide');
       $('.s3__more').addClass('hide');
    });
+   $('.more').slick({
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: false,
+      dots: true,
+      responsive: [
+         {
+           breakpoint: 1380,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 2
+           }
+         },
+         {
+            breakpoint: 1320,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+         },
+         {
+           breakpoint: 800,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         },
+         {
+           breakpoint: 300,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
+       ]
+    });
+    $('.inner').slick({
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+      responsive: [
+         {
+           breakpoint: 900,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           },
+           
+         }
+       ]
+    });
 });   
